93136f6d140ad5a6219f06747de7f5f605add315
valentinamzztt <83677469+valentinamzztt@users.noreply.github.com>
2023-01-21 10:54:15 -0800

    Add files via upload

---
+++ arduino/arduinoCodeCalib.ino
@@ -0,0 +1,39 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+ 
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 3
+ 
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+DeviceAddress insideThermometer = { 0x28, 0xAC, 0x15, 0x90, 0x0B, 0x00, 0x00, 0xB1};
+
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
+}
+ 
+ 
+void loop(void)
+{
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+  //Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("");
+
+  //Serial.print("Temperature is: ");
+  Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? 
+    // You can have more than one IC on the same bus. 
+    // 0 refers to the first IC on the wire
+    delay(50000);
+}
